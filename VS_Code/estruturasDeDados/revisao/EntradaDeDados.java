53bd4fe09e8cbe88320fe35a548b6fdc08981c1f
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-03-10 23:21:31 -0300

    feat: adiciona algoritmo que solicita nome e idade e imprime no terminal

---
+++ VS_Code/estruturasDeDados/revisao/EntradaDeDados.java
@@ -0,0 +1,18 @@
+package estruturasDeDados.revisao;
+import java.util.Scanner;
+
+public class EntradaDeDados {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Olá, "+ nome + "! Você tem " + idade + " anos.");
+
+        scanner.close();
+    }
+}
