c12cd4eb0c9e97bf097791bf3af0917fb6fe869d
LourencoMonte <f.lourenco13aps@gmail.com>
2025-08-17 18:03:52 -0300

    Adiciona arquivo contendo algoritmo sobre Operadore Relacionais

---
+++ Operadores_Basicos/OperadoresRelacionais.java
@@ -0,0 +1,14 @@
+package Estudo_Java.Operadores_Basicos;
+
+public class OperadoresRelacionais {
+    
+     public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Maior: " + (a > b));
+        System.out.println("Menor: " + (a < b));
+        System.out.println("Igual: " + (a == b));
+        System.out.println("Diferente: " + (a != b));
+     }
+}
