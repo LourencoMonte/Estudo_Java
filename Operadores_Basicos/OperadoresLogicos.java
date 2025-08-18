454ef11723ca95cf85b3a571e269c43fe56f01e8
LourencoMonte <f.lourenco13aps@gmail.com>
2025-08-18 10:00:11 -0300

    Adiciona arquivo com algoritmo sobre Operadores Lógicos

---
+++ Operadores_Basicos/OperadoresLogicos.java
@@ -0,0 +1,15 @@
+package Estudo_Java.Operadores_Basicos;
+
+public class OperadoresLogicos {
+    public static void main(String[] args){
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("x = true");
+        System.out.println("y = false");
+        System.out.println("x e y = " + (x && y));
+        System.out.println("x ou y = " + (x || y));
+        System.out.println("x e y = " + (x && y));
+    }
+    
+}
