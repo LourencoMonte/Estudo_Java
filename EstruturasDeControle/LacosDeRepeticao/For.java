606a808a38fd3be3a5fd52451ac27fb0840a93e2
LourencoMonte <f.lourenco13aps@gmail.com>
2025-08-21 11:19:21 -0300

    Adiciona pasta LacosDeRepeticao contendo o algoritmo For

---
+++ EstruturasDeControle/LacosDeRepeticao/For.java
@@ -0,0 +1,12 @@
+package Estudo_Java.EstruturasDeControle.LacosDeRepeticao;
+
+public class For {
+    public static void main(String[] args){
+        System.out.println("Conte até 9");
+
+        for (int i = 0; i <= 9; i++) {
+        System.out.println(i);
+        }
+    }
+    
+}
