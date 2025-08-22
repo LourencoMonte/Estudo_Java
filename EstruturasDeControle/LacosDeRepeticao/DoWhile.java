6f2409fdb220533e6008da820f1a0b7d8297fc70
LourencoMonte <f.lourenco13aps@gmail.com>
2025-08-22 14:46:59 -0300

    Adiciona algoritmo sobre Do While

---
+++ EstruturasDeControle/LacosDeRepeticao/DoWhile.java
@@ -0,0 +1,12 @@
+package Estudo_Java.EstruturasDeControle.LacosDeRepeticao;
+
+public class ForWhiile {
+    public static void main(String[] args){
+        int k = 1;
+do {
+    System.out.println("Executa pelo menos uma vez: " + k);
+    k++;
+} while (k <= 3);
+    }
+    
+}
