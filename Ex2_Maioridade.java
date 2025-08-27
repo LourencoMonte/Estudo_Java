ae0f69e17b10b7cdcfa7d188ba69e641c204b334
LourencoMonte <f.lourenco13aps@gmail.com>
2025-08-27 17:58:21 -0300

    2° exercício

---
+++ Ex2_Maioridade.java
@@ -0,0 +1,20 @@
+package Estudo_Java;
+import java.util.Scanner;
+
+public class Ex2_Maioridade {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        if (idade >= 18){
+            System.out.println("O usuário é maior de idade.");
+        } else {
+                System.out.println("O usuário é menor de idade.");
+            }
+
+        sc.close();
+    }
+    
+}
