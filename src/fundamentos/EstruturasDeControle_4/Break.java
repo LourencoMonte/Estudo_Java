f14fb3b2e8db7acf307bf14728e0f4300cc2fdb8
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-10-22 10:57:34 -0300

    feat: Adiciona código com função break

--- src/fundamentos/EstruturasDeControle_4/Break.java
+++ src/fundamentos/EstruturasDeControle_4/Break.java
@@ -3,14 +3,22 @@ import java.util.Scanner;
 
 public class Break {
     public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int numeroSecreto = 7;
-    int num;
+        Scanner sc = new Scanner(System.in);
+        int numeroSecreto = 7;
 
-while(num != numeroSecreto) {
-    System.out.println("Digite o número secreto para sair dp loop");
-    int num = sc.nextInt();
-        } 
+    while(true) {
+        System.out.println("Digite o número secreto para sair do loop");
+        int num = sc.nextInt();
+
+        if (num == numeroSecreto){
+            System.out.println("Vocẽ acertou o número!");
+            break;
+        }
+
+        System.out.println("Errado! Tente novamente.\n");
+
+        }
+        sc.close(); 
     }
 }
 
