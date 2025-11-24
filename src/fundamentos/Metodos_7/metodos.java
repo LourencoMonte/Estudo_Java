4f956d446daa3d54448c8e4ea0ac0424fa009587
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-11-24 11:04:57 -0300

    Método simples de chamada de cumprimento

---
+++ src/fundamentos/Metodos_7/metodos.java
@@ -0,0 +1,16 @@
+package fundamentos.Metodos_7;
+
+public class metodos {
+ 
+        
+
+        public static void mostrarMensagem(String nome) {
+            System.out.println("Olá " + nome);
+        }
+    
+    
+   public static void main(String[] args) {
+        mostrarMensagem("Lourenço");
+   }
+}
+
