4e4b285b3ec906c07d95c0aaaaccdf61d7e1763a
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-11-24 11:46:15 -0300

    Ajustes

--- src/fundamentos/Metodos_7/metodos.java
+++ src/fundamentos/Metodos_7/metodos.java
@@ -1,16 +1,14 @@
 package fundamentos.Metodos_7;
 
-public class metodos {
- 
-        
+public class metodos{
 
-        public static void mostrarMensagem(String nome) {
-            System.out.println("Olá " + nome);
-        }
-    
-    
-   public static void main(String[] args) {
-        mostrarMensagem("Lourenço");
-   }
+     /* Métodos */
+    static void mostrarNome(String nome){
+        System.out.println("Olá " + nome);
+    }
+
+    /* Método main */
+    public static void main(String[] args){
+        mostrarNome("Lourenço");
+    }
 }
-
