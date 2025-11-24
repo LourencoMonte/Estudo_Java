c09814855e077605abdc1ca507067bcaa8ad6532
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-11-24 11:58:55 -0300

    feat: Inclusão de códito demonstrativo de método sem retorno e parâmetro

---
+++ src/fundamentos/Metodos_7/metodo3.java
@@ -0,0 +1,13 @@
+package fundamentos.Metodos_7;
+
+/* Método sem retorno e parâmetro */
+public class metodo3 {
+    public static void mensagem() {
+        System.out.println("Hello word");
+    }
+
+    public static void main(String[] args) {
+        mensagem();
+    }
+    
+}
