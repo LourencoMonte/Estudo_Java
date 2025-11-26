98a2b97ca21f1a4b009ca5ec7818487f03098911
Lourenco Monte <f.lourenco13aps@gmail.com>
2025-11-26 09:37:13 -0300

    feat: Inicia código para estudo de sobrecarga de métodos(Method Overloading), usando o exemplo de calculadora simple. Iniciando com método soma

---
+++ src/fundamentos/Metodos_7/sobrecargaDeMetodo.java
@@ -0,0 +1,30 @@
+package fundamentos.Metodos_7;
+import java.util.Scanner;
+
+public class sobrecargaDeMetodo {
+    Scanner sc = new Scanner(System.in);
+
+    /*Métodos com sobrecarga*/
+
+    /* somar */
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+    public static int somar(int a, int b, int c) {
+        return a + b + c;
+    }
+    public static double somar(double a, double b){
+        return a + b;
+    }
+    public static double somar(double a, double b, double c){
+        return a + b + c;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Resultado: " + somar(10, 20));
+        System.out.println("Resultado: " + somar(10.5, 20));
+        System.out.println("Resultado: " + somar(10, 20, 30));
+        
+    }
+    
+}
