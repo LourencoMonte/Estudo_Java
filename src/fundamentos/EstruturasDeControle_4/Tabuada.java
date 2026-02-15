3a828ce670b2ba5d1ab79665a5224a7f9a15e552
Lourenco Monte <f.lourenco13aps@gmail.com>
2026-02-15 16:21:25 -0300

    feat: Adiciona algoritmo que cria tabuada de númeri digitado pelo usuário

---
+++ src/fundamentos/EstruturasDeControle_4/Tabuada.java
@@ -0,0 +1,19 @@
+package fundamentos.EstruturasDeControle_4;
+import java.util.Scanner;
+
+public class Lista {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número para receber a tabuada completa do mesmo.");
+        int tab = sc.nextInt();
+        System.out.println("\nTabuada do " + tab + ":\n");
+
+       for(int i = 0; i <= 10; i++){
+            System.out.println(tab + "x" + i + "=" + tab * i);
+       }
+       
+
+       sc.close();
+    }
+}
