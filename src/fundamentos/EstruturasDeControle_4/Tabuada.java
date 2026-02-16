747a77e939b64a29a3dff15a2036103aac4c76b8
Lourenco Monte <f.lourenco13aps@gmail.com>
2026-02-16 15:55:11 -0300

    refactor: corrigir nome da classe

--- src/fundamentos/EstruturasDeControle_4/Tabuada.java
+++ src/fundamentos/EstruturasDeControle_4/Tabuada.java
@@ -1,7 +1,7 @@
 package fundamentos.EstruturasDeControle_4;
 import java.util.Scanner;
 
-public class Lista {
+public class Tabuada {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
