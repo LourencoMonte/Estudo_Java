fdfa97526a4d300df53e47404d562a15acbbf706
Lourenco Monte <f.lourenco13aps@gmail.com>
2026-02-16 18:25:18 -0300

    feat: inicia algoritmo para retornar numeros immpares ou pares. De acordo com a seleção e parametros do usuário

---
+++ src/fundamentos/EstruturasDeControle_4/ex3.java
@@ -0,0 +1,31 @@
+package fundamentos.EstruturasDeControle_4;
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Neste código você vai digitar um número, e logo após outro número maior.");
+
+            System.out.println("Digite o número piso");
+            int numPiso = sc.nextInt();
+
+            System.out.println("Agora digite o número teto");
+            int numTeto = sc.nextInt();
+
+            System.out.println("Digite (1) para receber os números Ímpares ou (2) para receber os números Pares.");
+            int escolha = sc.nextInt();
+
+            switch (escolha){
+            case 1:
+                for(int i = numTeto; i >= numPiso; i--){
+                    System.out.println(i);
+                }
+            
+            case 2:
+                for(int i = numTeto; i >= numPiso; i--){
+                    System.out.println(i);
+                }
+            } 
+        sc.close();
+    }
+}
